558933cca964eb1b41f49f2a66a1b8c04375f8a5
paulino-s <paulinovasquez900@gmail.com>
2021-06-19 20:52:44 -0600

    registrar estudiante

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+const router = require("./routes.js");
+
+/*CONFIGURACIONES*/
+
+app.set("port", 3000);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
+
+app.listen(app.get("port"), () => {
+  console.log("Server status 200 on port 3000");
+});
---
+++ server/routes.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const router = Router();
+const { query } = require("./db");
+const { registrarEstudiante } = require("./controllers/registerController");
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Probando servidor",
+  });
+});
+
+router.get("/fromoracle", async (req, res) => {
+  const login = [];
+  sql = "select * from empresa";
+
+  let result = await DB.Open(sql, [], false);
+
+  //console.log(result.rows);
+
+  console.log(login);
+  result.rows.map((person) => {
+    let userSchema = {
+      IDLOGIN: person[0],
+      USERNAME: person[1],
+      PASSWORD: person[2],
+      PASSWORD: person[3],
+      PASSWORD: person[4],
+    };
+    login.push(userSchema);
+  });
+  res.json({ login });
+});
+
+router.post("/registrar-estudiante", registrarEstudiante);
+
+module.exports = router;
